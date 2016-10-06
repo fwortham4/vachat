7347577784d7196cb9453b067cf1a843a420701b
fwortham4 <forrest.wortham@gmail.com>
2016-10-06 00:18:24 -0500

    Fixes migration problem.

--- vachat/db/migrate/20161006043211_create_messages.rb
+++ vachat/db/migrate/20161006043211_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.integer :sender_id, null: false
-      # t.integer :chatroom_id, null: false
+      t.integer :user_id, null: false
+      t.integer :chatroom_id, null: false
       t.text :content, null: false
 
       t.timestamps(null: false)
--- vachat/db/schema.rb
+++ vachat/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006014124) do
+ActiveRecord::Schema.define(version: 20161006043218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,10 @@ ActiveRecord::Schema.define(version: 20161006014124) do
 
   create_table "messages", force: :cascade do |t|
     t.integer  "sender_id",   null: false
+    t.integer  "chatroom_id", null: false
     t.text     "content",     null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "chatroom_id"
   end
 
   create_table "users", force: :cascade do |t|
