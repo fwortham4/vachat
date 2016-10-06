9e5a052d013005b661a49a39d1cb1df5e0258aab
fwortham4 <forrest.wortham@gmail.com>
2016-10-06 16:13:43 -0500

    Minor changes.

--- vachat/app/channels/application_cable/chatroom_channel.rb
+++ vachat/app/channels/application_cable/chatroom_channel.rb
@@ -3,4 +3,8 @@ class ChatChannel < ApplicationCable::Channel
     # What goes here????
     stream_from "chatroom"
   end
+
+  def receive(data)
+    ActionCable.server.broadcast "messages", collection
+  end
 end
