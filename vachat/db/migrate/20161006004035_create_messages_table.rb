fd6a3c31b5b9721e0f8db4febfc9b8b7580e3a57
fwortham4 <forrest.wortham@gmail.com>
2016-10-05 19:42:07 -0500

    Generates migration for users, messages, and chatrooms.

---
+++ vachat/db/migrate/20161006004035_create_messages_table.rb
@@ -0,0 +1,6 @@
+class CreateMessagesTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages_tables do |t|
+    end
+  end
+end
---
+++ vachat/db/migrate/20161006004051_create_chatrooms_table.rb
@@ -0,0 +1,6 @@
+class CreateChatroomsTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :chatrooms_tables do |t|
+    end
+  end
+end
