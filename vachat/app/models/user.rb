47b0736e3c840f755dc3264338cc61bbc22b1d2a
Ben Newman & Forrest W <pair+bjnewman.fwortham4@devbootcamp.com>
2016-10-06 18:28:19 -0500

    configure devise for users

--- vachat/app/models/user.rb
+++ vachat/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   has_many :messages, foreign_key: :sender_id
   has_many :chatrooms, through: :messages
   validates :email, presence: true, uniqueness: true
