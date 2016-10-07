24120a1112da5796410cbc2c401c5bc0deb68667
Ben Newman & Forrest W <pair+bjnewman.fwortham4@devbootcamp.com>
2016-10-06 19:17:25 -0500

    first try at google omniauth

--- vachat/app/models/user.rb
+++ vachat/app/models/user.rb
@@ -2,8 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
   has_many :messages, foreign_key: :sender_id
   has_many :chatrooms, through: :messages
   validates :email, presence: true, uniqueness: true
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+    user
+  end
 end
